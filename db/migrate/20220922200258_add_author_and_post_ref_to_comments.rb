5c1585ee65c96471447c197101738ec540931822
Gabriel Santo <gvgesanto2@gmail.com>
2022-09-22 17:06:55 -0300

    fix: Fix typo in the AddAuthorAndPostRefToComments migration

--- db/migrate/20220922200258_add_author_and_post_ref_to_comments.rb
+++ db/migrate/20220922200258_add_author_and_post_ref_to_comments.rb
@@ -1,6 +1,6 @@
 class AddAuthorAndPostRefToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :author, null: false, foreign_key: { to_table: 'users' }
-    add_reference :likes, :post, null: false, foreign_key: { to_table: 'posts' }
+    add_reference :comments, :author, null: false, foreign_key: { to_table: 'users' }
+    add_reference :comments, :post, null: false, foreign_key: { to_table: 'posts' }
   end
 end
