1155e3fc855811334ac0d42b08f51fea88018c03
Gabriel Santo <gvgesanto2@gmail.com>
2022-09-23 18:02:28 -0300

    fix: Fix a problem with the posts, comments, and likes associations in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def get_most_recent_posts(num = nil)
-    recent_posts = posts.by_author(self).most_recent_ones
+    recent_posts = posts.most_recent_ones
     num ? recent_posts.limit(num) : recent_posts
   end
 end
