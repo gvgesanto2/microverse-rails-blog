7285b88a18996f024758eba544fc33e733d70cc3
Gabriel Santo <gvgesanto2@gmail.com>
2022-09-23 17:15:09 -0300

    feat: Add the Like and Comment models

---
+++ app/models/comment.rb
@@ -0,0 +1,15 @@
+class Comment < ApplicationRecord
+  belongs_to author
+  belongs_to post
+
+  scope :by_post, -> (post_id) { where(post_id: post_id) }
+  scope :most_recent_ones, -> { order('created_at DESC') }
+
+  after_save :update_comments_counter
+
+  private 
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,16 @@ class Post < ApplicationRecord
   scope :by_author, -> (author_id) { where(author_id: author_id) }
   scope :most_recent_ones, -> { order('created_at DESC') }
 
+  after_save :update_posts_counter
+
   def get_most_recent_comments(num = nil)
     recent_comments = comments.by_post(self).most_recent_ones
     num ? recent_comments.limit(num) : recent_comments
   end
 
-  def update_post_counter
+  private
+
+  def update_posts_counter
     author.increment!(:posts_counter)
   end
 end
