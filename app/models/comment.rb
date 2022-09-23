9da08e0b14401b93ac9d4479761fcbb668476850
Gabriel Santo <gvgesanto2@gmail.com>
2022-09-23 17:26:58 -0300

    fix: Fix the NameError problem for the Author association in the Comment, Like, and Post models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to author
-  belongs_to post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
   scope :by_post, -> (post_id) { where(post_id: post_id) }
   scope :most_recent_ones, -> { order('created_at DESC') }
