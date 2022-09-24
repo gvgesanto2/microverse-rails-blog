e0925c9804e800c35ba2e541fe30e47c5d019aa0
Gabriel Santo <gvgesanto2@gmail.com>
2022-09-24 16:30:43 -0300

    fix: Fix linters errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,45 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject do 
-    @fake_user = User.new(name: 'Richard', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Brazil.')
-    Post.create(title: 'testing', author: @fake_user, text: 'testing') 
+  subject do
+    @fake_user = User.new(name: 'Richard', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Teacher from Brazil.')
+    Post.create(title: 'testing', author: @fake_user, text: 'testing')
   end
 
   before { subject.save }
 
-  it 'should be valid with valid attributes' do    
+  it 'should be valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'should NOT be valid with a non-existing author' do 
+  it 'should NOT be valid with a non-existing author' do
     subject.author = nil
     expect(subject).to_not be_valid
   end
 
-  it 'should NOT be valid without a title' do 
+  it 'should NOT be valid without a title' do
     subject.title = nil
     expect(subject).to_not be_valid
   end
 
-  it 'should NOT be valid with a title longer than 250 characters long' do 
+  it 'should NOT be valid with a title longer than 250 characters long' do
     subject.title = 'a' * 251
     expect(subject).to_not be_valid
   end
 
-  it 'likes_counter should be initially set to zero' do    
+  it 'likes_counter should be initially set to zero' do
     expect(subject.likes_counter).to be(0)
   end
 
-  it 'likes_counter should be equal to the number of comments created' do    
-    NUM_LIKES_CREATED = 3
+  it 'likes_counter should be equal to the number of comments created' do
+    num_likes_created = 3
 
-    NUM_LIKES_CREATED.times { Like.create(author: @fake_user, post: subject) }
+    num_likes_created.times { Like.create(author: @fake_user, post: subject) }
 
-    expect(subject.likes_counter).to be(NUM_LIKES_CREATED)
+    expect(subject.likes_counter).to be(num_likes_created)
   end
 
-  it 'should NOT be valid with a non-integer likes_counter' do 
+  it 'should NOT be valid with a non-integer likes_counter' do
     subject.likes_counter = 1.5
     expect(subject).to_not be_valid
 
@@ -47,24 +48,24 @@ RSpec.describe Post, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'should NOT be valid with a likes_counter less than zero' do 
+  it 'should NOT be valid with a likes_counter less than zero' do
     subject.likes_counter = -1
     expect(subject).to_not be_valid
   end
 
-  it 'comments_counter should be initially set to zero' do    
+  it 'comments_counter should be initially set to zero' do
     expect(subject.comments_counter).to be(0)
   end
 
-  it 'comments_counter should be equal to the number of comments created' do    
-    NUM_COMMENTS_CREATED = 3
+  it 'comments_counter should be equal to the number of comments created' do
+    num_comments_created = 3
 
-    NUM_COMMENTS_CREATED.times { Comment.create(post: subject, author: @fake_user, text: 'testing') }
+    num_comments_created.times { Comment.create(post: subject, author: @fake_user, text: 'testing') }
 
-    expect(subject.comments_counter).to be(NUM_COMMENTS_CREATED)
+    expect(subject.comments_counter).to be(num_comments_created)
   end
 
-  it 'should NOT be valid with a non-integer comments_counter' do 
+  it 'should NOT be valid with a non-integer comments_counter' do
     subject.comments_counter = 1.5
     expect(subject).to_not be_valid
 
@@ -72,7 +73,7 @@ RSpec.describe Post, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'should NOT be valid with a comments_counter less than zero' do 
+  it 'should NOT be valid with a comments_counter less than zero' do
     subject.comments_counter = -1
     expect(subject).to_not be_valid
   end
@@ -83,18 +84,18 @@ RSpec.describe Post, type: :model do
   end
 
   it 'get_most_recent_comments should return the most recent posts created by the user' do
-    NUM_COMMENTS_CREATED = 5
-    NUM_RECENT_COMMENTS = 3
+    num_comments_created = 5
+    num_recent_comments = 3
     comments_created = []
 
-    NUM_COMMENTS_CREATED.times do
+    num_comments_created.times do
       comments_created << Comment.create(post: subject, author: @fake_user, text: 'testing')
     end
 
-    expect(subject.get_most_recent_comments.length).to be(NUM_COMMENTS_CREATED)
+    expect(subject.get_most_recent_comments.length).to be(num_comments_created)
 
-    recent_comments = comments_created[NUM_RECENT_COMMENTS - 1..-1].reverse
+    recent_comments = comments_created[num_recent_comments - 1..].reverse
 
-    expect(subject.get_most_recent_comments(NUM_RECENT_COMMENTS)).to eq(recent_comments)
+    expect(subject.get_most_recent_comments(num_recent_comments)).to eq(recent_comments)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,20 +5,20 @@ RSpec.describe User, type: :model do
 
   before { subject.save }
 
-  it 'should be valid with valid attributes' do    
+  it 'should be valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'should NOT be valid without a name' do 
+  it 'should NOT be valid without a name' do
     subject.name = nil
     expect(subject).to_not be_valid
   end
 
-  it 'posts_counter should be initially set to zero' do    
+  it 'posts_counter should be initially set to zero' do
     expect(subject.posts_counter).to be(0)
   end
 
-  it 'should NOT be valid with a non-integer posts_counter' do 
+  it 'should NOT be valid with a non-integer posts_counter' do
     subject.posts_counter = 1.5
     expect(subject).to_not be_valid
 
@@ -26,7 +26,7 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'should NOT be valid with a posts_counter less than zero' do 
+  it 'should NOT be valid with a posts_counter less than zero' do
     subject.posts_counter = -1
     expect(subject).to_not be_valid
   end
@@ -37,18 +37,18 @@ RSpec.describe User, type: :model do
   end
 
   it 'get_most_recent_posts should return the most recent posts created by the user' do
-    NUM_POSTS_CREATED = 5
-    NUM_RECENT_POSTS = 3
+    num_posts_created = 5
+    num_recent_posts = 3
     posts_created = []
 
-    NUM_POSTS_CREATED.times do
+    num_posts_created.times do
       posts_created << Post.create(title: 'testing', author: subject, text: 'testing')
     end
 
-    expect(subject.get_most_recent_posts.length).to be(NUM_POSTS_CREATED)
+    expect(subject.get_most_recent_posts.length).to be(num_posts_created)
 
-    recent_posts = posts_created[NUM_RECENT_POSTS - 1..-1].reverse
+    recent_posts = posts_created[num_recent_posts - 1..].reverse
 
-    expect(subject.get_most_recent_posts(NUM_RECENT_POSTS)).to eq(recent_posts)
+    expect(subject.get_most_recent_posts(num_recent_posts)).to eq(recent_posts)
   end
 end
