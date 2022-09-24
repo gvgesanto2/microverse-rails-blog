f3b5b13a99705307761c85b579a3910ca967d88b
Gabriel Santo <gvgesanto2@gmail.com>
2022-09-24 16:17:12 -0300

    test: Add tests for the Comment model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  subject do
+    fake_user = User.new(name: 'User Test', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Brazil.')
+    fake_post = Post.create(title: 'testing', author: fake_user, text: 'testing') 
+    Comment.new(author: fake_user, post: fake_post, text: 'testing')
+  end
+
+  before { subject.save }
+
+  it 'should be valid with valid attributes' do    
+    expect(subject).to be_valid
+  end
+
+  it 'should NOT be valid with an invalid author' do 
+    subject.author = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should NOT be valid with an invalid post' do 
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+end
