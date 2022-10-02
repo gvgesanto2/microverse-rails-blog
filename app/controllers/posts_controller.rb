47fc4e7109acb6c78091e810d69e7e506fd4b9ab
Gabriel Santo <gvgesanto2@gmail.com>
2022-10-01 21:20:41 -0300

    feat: Add the create action to the PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,15 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def create
+    post = current_user.posts.new(strong_params)
+    redirect_to user_path(current_user) if post.save
+  end
+
+  private
+
+  def strong_params
+    params.require(:user_posts).permit(:title, :text)
+  end
 end
